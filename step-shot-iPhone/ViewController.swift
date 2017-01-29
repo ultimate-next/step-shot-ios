458e18b11819d17ef96b6f2ef4bf332f937e33d2
okoysm <fly.child1017@gmail.com>
2017-01-29 14:19:03 +0900

    move to step count program

--- step-shot-iPhone/ViewController.swift
+++ step-shot-iPhone/ViewController.swift
@@ -7,17 +7,37 @@
 //
 
 import UIKit
+import Foundation
+import CoreMotion
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        stepLabel.text = "Hello World"
+        startStepCounting()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    let pedometer = CMPedometer()
+    @IBOutlet var stepLabel: UILabel!
+
+    func startStepCounting() {
+        if CMPedometer.isStepCountingAvailable() {
+            self.pedometer.startUpdates(from: Date(), withHandler: {
+                [weak self] (data: CMPedometerData?, error: NSError?) -> Void in
+                DispatchQueue.main.async(execute: {
+                    if data != nil && error == nil {
+                        self?.stepLabel.text = "step: \(data!.numberOfSteps)"
+                    }
+                })
+                } as! CMPedometerHandler)
+        }
     }
     
 }
