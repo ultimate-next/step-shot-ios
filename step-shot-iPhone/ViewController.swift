d4376203f9f767381aa6aaac3721d8dc2a1b9f19
okoysm <fly.child1017@gmail.com>
2017-01-29 10:45:50 +0900

    initial commit

---
+++ step-shot-iPhone/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  step-shot-iPhone
+//
+//  Created by Syoko Matsumura on 2017/01/29.
+//  Copyright © 2017年 ultimate-next. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
