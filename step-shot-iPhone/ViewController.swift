dc9f4584050aebb40979bc809a313fc233e82d7e
ym <kiyoshi.kodama@gmail.com>
2017-02-02 01:24:48 +0900

    Fix update logic of step count.

--- step-shot-iPhone/ViewController.swift
+++ step-shot-iPhone/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        stepLabel.text = "Hello World"
+        stepLabel.text = "0 歩"
         startStepCounting()
     }
     
@@ -28,18 +28,21 @@ class ViewController: UIViewController {
     @IBOutlet var stepLabel: UILabel!
   
     func startStepCounting() {
-        pedometer.startUpdates(from: NSDate() as Date, withHandler: { (data, error) -> Void in
-            if error==nil {
-                let myStep = data!.numberOfSteps
-                
-                
-                
-                self.stepLabel.text = "\(myStep) 歩"
-            }
+        let now = Date()    // 現在時間
+        let calendar = Calendar(identifier: .gregorian)
+        let components = calendar.dateComponents([.year, .month, .day], from: now)
+        let today = calendar.date(from: components) // 今日の0時
+        
+        self.pedometer.startUpdates(from: today!, withHandler: {
+            (data: CMPedometerData?, error: Error?) in
+            DispatchQueue.main.async(execute: { () in
+                guard let exData = data, error == nil else {
+                    // 取得できなかった場合ここで抜ける
+                    return
+                }
+                // 歩数取得
+                self.stepLabel.text = "\(exData.numberOfSteps) 歩"
+            })
         })
     }
-    
-    
-    
 }
-
