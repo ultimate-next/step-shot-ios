5b52b2b3c9d5398c4c43cad9885b6f6f54266d2c
ym <kiyoshi.kodama@gmail.com>
2017-02-05 15:06:59 +0900

    Play sound when notified

--- step-shot-iPhone/ViewController.swift
+++ step-shot-iPhone/ViewController.swift
@@ -19,6 +19,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         // Do any additional setup after loading the view, typically from a nib.
         stepLabel.text = "0 歩"
         
+        applyDateLocale() // 日付出力時のフォーマットをJSTにする
+        loadCurrentLevel() // 前回終了時までに達成したレベルをロード
+        loadCurrentDate() // 前回終了時の日付をロード
+        correctCurrentLevelAndDate() //日付が変わっている場合、current_lavelを0にする
         prepareStepLevelMap() // 歩数のレベルアップ表を準備
         startStepCounting() // 歩数カウントの有効化
         enableNotification() // ローカル通知を使えるようにする
@@ -34,27 +38,71 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     var locationManager: CLLocationManager!
     var step_count: Int = 0
     var current_level: Int = 0
+    var current_date: String!
     var step_level_map: [Int] = []
+    let sounds: [String] = ["cheer1.mp3", "cheer2.mp3", "cheer3.mp3"]
+    let userDefaults = UserDefaults.standard
+    let dateFormatter = DateFormatter()
     
     @IBOutlet var stepLabel: UILabel!
+    @IBOutlet var levelLabel: UILabel!
     
+    
+    func setCurrentLevel(level: Int) {
+        current_level = level
+        levelLabel.text = "現在のレベル：\(current_level)"
+    }
+
     /* ------------------------------
         歩数関連の処理
      --------------------------------*/
+    
+    func applyDateLocale() {
+        dateFormatter.locale = Locale(identifier: "ja_JP")
+        dateFormatter.dateStyle = .short
+    }
 
+    func loadCurrentLevel() {
+        if (userDefaults.object(forKey: "StepCount") != nil) {
+            setCurrentLevel(level: userDefaults.integer(forKey: "StepCount"))
+        } else {
+            setCurrentLevel(level: 0)
+        }
+    }
+    
+    func loadCurrentDate() {
+        print("today: \(dateFormatter.string(from: calcToday()))")
+        if (userDefaults.object(forKey: "CurrentDate") != nil) {
+            current_date = userDefaults.string(forKey: "CurrentDate")
+        } else {
+            current_date = dateFormatter.string(from: calcToday())
+        }
+    }
+    
+    func correctCurrentLevelAndDate() {
+        if(current_date != dateFormatter.string(from: calcToday())) {
+            setCurrentLevel(level: 0)
+            current_date = dateFormatter.string(from: calcToday())
+            userDefaults.set(current_level, forKey: "StepCount")
+            userDefaults.set(current_date, forKey: "CurrentDate")
+        }
+    }
+    
     func prepareStepLevelMap() {
         for i in 1..<1000 {
             step_level_map.append(i * 50)
         }
     }
     
-    func startStepCounting() {
+    func calcToday() -> Date {
         let now = Date()    // 現在時間
         let calendar = Calendar(identifier: .gregorian)
         let components = calendar.dateComponents([.year, .month, .day], from: now)
-        let today = calendar.date(from: components) // 今日の0時
-        
-        self.pedometer.startUpdates(from: today!, withHandler: {
+        return calendar.date(from: components)! // 今日の0時
+    }
+    
+    func startStepCounting() {
+        self.pedometer.startUpdates(from: calcToday(), withHandler: {
             (data: CMPedometerData?, error: Error?) in
             DispatchQueue.main.async(execute: { () in
                 guard let exData = data, error == nil else {
@@ -69,13 +117,17 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     func checkStepLevelUp() -> Bool {
-        if(step_level_map[current_level] <= step_count) {
-            current_level += 1
-            return true
+        var level_up:Bool = false
+        while(step_level_map[current_level] <= step_count) {
+            setCurrentLevel(level: current_level + 1)
+            level_up = true
+            userDefaults.set(current_level, forKey: "StepCount")
+            print("現在のレベル: \(current_level)")
         }
-        return false
+        return level_up
     }
 
+    
     /* ------------------------------
         ローカル通知関連の処理
      --------------------------------*/
@@ -100,8 +152,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         let content = UNMutableNotificationContent()
         content.title = "step-shot"
         content.subtitle = "おめでとうございます！"
-        content.body = "\(step_count)歩、歩きました！"
-        content.sound = UNNotificationSound.default()
+        content.body = "\(step_level_map[current_level - 1])歩、歩きました！"
+        content.sound = UNNotificationSound.init(named: sounds[Int(arc4random_uniform(3))])
         
         // 1秒後に発火(強制的にローカル通知を発火させる)
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
@@ -145,6 +197,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 print("Error: location update")
                 return
         }
+        
+        correctCurrentLevelAndDate()
         
         if checkStepLevelUp() {
             reserveNotification()
